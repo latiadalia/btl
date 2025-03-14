066f17575fc08e5a85c0ba8afe287e0161fbcb11
“latiadalia” <lamnguyen.work03@gmail.com>
2025-03-14 18:31:36 +0700

    Xóa các file không cần thiết

---
+++ demoMVC/Controllers/PersonController.cs
@@ -0,0 +1,121 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.EntityFrameworkCore;
+using DemoMVC.Data;
+using DemoMVC.Models;
+
+namespace DemoMVC.Controllers
+{
+     
+    public class PersonController(ApplicationDbContext context) : Controller
+    {
+
+        private readonly ApplicationDbContext _context = context;
+
+        public async Task<IActionResult> Index()
+        {
+            var model = await _context.Persons.ToListAsync();
+            return View(model);
+        }
+        public IActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("PersonId,Fullname,Address")]Person person)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(person);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(person);
+
+        }
+        public async Task<IActionResult> Edit(string id)
+        {
+            if (id == null || _context.Persons == null)
+            {
+                return NotFound();
+            }
+
+            var person = await _context.Persons.FindAsync(id);
+            if (person == null)
+            {
+                return NotFound();
+            } 
+            return View(person);                                       
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult>Edit(string id, [Bind("PersonId,FUllname,Address")]Person person)
+        {
+            if(id != person.PersonId)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(person);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if(!PersonExitsts(person.PersonId))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(person);
+        }
+
+        private bool PersonExitsts(string personId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<IActionResult>Delete(string id)
+        {
+            if (id == null || _context.Persons == null)
+            {
+                return NotFound();
+            }
+
+            var person = await _context.Persons.FirstOrDefaultAsync(m => m.PersonId == id);
+            if (person == null)
+            {
+                return NotFound();
+            }
+
+            return View(person);
+        }
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var person = await _context.Persons.FindAsync(id);
+            _context.Persons.Remove(person);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool PersonExists(string id)
+        {
+            return (_context.Persons?.Any(e => e.PersonId == id)).GetValueOrDefault();
+        }
+    }
+}
+    
+
+    
